c063a122fa8219c2ca2957692e9f279a1032480e
Michael Hartung <hartungm4@gmail.com>
2015-02-16 21:24:55 -0500

    modified user spec

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,52 +1,55 @@
-beforeEach(function() {
-	var user = User.init("Bob", 17.32, "Dev");
-});
+describe("User Specifications", function() {
 
-describe("Initalized user Bob", function() {
-	it("Users name is Bob", function() {
-		expect(user.userName).toBe("Bob");
+	beforeEach(function() {
+		var user = User.init("Bob", 17.32, "Dev");
 	});
-	it("Users payrate is $17.32", function() {
-		expect(user.payRate).toBe(17.32);
-	});
-	it("Users role is Dev", function() {
-		expect(user.role).toBe("Dev");
-	});
-	it("Users hours worked should start at 0", function() {
-		expect(user.hoursWorked).toBe(0);
-	});
-});
 
-describe("User changed Name", function(){
-	it("Bob changed his name to Sal", function() {
-		user.setName("Sal");
-		expect(user.userName).toBe("Sal");
+	describe("Initalized user Bob", function() {
+		it("Users name is Bob", function() {
+			expect(user.userName).toBe("Bob");
+		});
+		it("Users payrate is $17.32", function() {
+			expect(user.payRate).toBe(17.32);
+		});
+		it("Users role is Dev", function() {
+			expect(user.role).toBe("Dev");
+		});
+		it("Users hours worked should start at 0", function() {
+			expect(user.hoursWorked).toBe(0);
+		});
 	});
-});
 
-describe("User changed payRate", function() {
-	it("Bob: pay increase to $20.00", function(){
-		user.setPayRate(20);
-		expect(user.payRate).toBe(20);
+	describe("User changed Name", function(){
+		it("Bob changed his name to Sal", function() {
+			user.setName("Sal");
+			expect(user.userName).toBe("Sal");
+		});
 	});
-});
 
-describe("User changed roles", function() {
-	it("Bob decided to be a Scrum Master", function() {
-		user.setRole("SM");
-		expect(user.role).toBe("SM");
+	describe("User changed payRate", function() {
+		it("Bob: pay increase to $20.00", function(){
+			user.setPayRate(20);
+			expect(user.payRate).toBe(20);
+		});
 	});
-});
 
-describe("User worked some hours", function() {
-	it("Bob worked 18.5 hours" function() {
-		user.addHoursWorked(18.5);
-		expect(user.hoursWorked).toBe(18.5);
+	describe("User changed roles", function() {
+		it("Bob decided to be a Scrum Master", function() {
+			user.setRole("SM");
+			expect(user.role).toBe("SM");
+		});
 	});
-	it("Bob worked 18.5 hours, then worked 20 more hours" function() {
-		user.addHoursWorked(18.5);
-		expect(user.hoursWorked).toBe(18.5);
-		user.addHoursWorked(20);
-		expect(user.hoursWorked).toBe(20);
+
+	describe("User worked some hours", function() {
+		it("Bob worked 18.5 hours", function() {
+			user.addHoursWorked(18.5);
+			expect(user.hoursWorked).toBe(18.5);
+		});
+		it("Bob worked 18.5 hours, then worked 20 more hours", function() {
+			user.addHoursWorked(18.5);
+			expect(user.hoursWorked).toBe(18.5);
+			user.addHoursWorked(20);
+			expect(user.hoursWorked).toBe(20);
+		});
 	});
 });
