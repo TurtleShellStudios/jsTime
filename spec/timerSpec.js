52308ebd5b286cbe4d29991727ea7f1ac878b6e7
Michael Hartung <hartungm4@gmail.com>
2015-02-17 09:01:27 -0500

    Started timerSpec

---
+++ spec/timerSpec.js
@@ -0,0 +1,27 @@
+describe("Timer Specifications", function() {
+
+	beforeEach(function() {
+		var timer = Timer.init()
+	});
+
+	describe("Initialize Tests", function() {
+		it("Timer initialized to 0", function() {
+			expect(timer.elapsedTime()).toBe(0);
+		});
+		it("Timer initialized as stopped", function() {
+			expect(timer.isStarted).toBe(false);
+		});
+		it("Timer label's value intialized as 00:00:00", function() {
+			expect(timer.timeLabel).toBe('00:00:00');
+		});
+	});
+
+	describe("Setting a task", function() {
+		beforeEach(function() {
+			var task = Task.init("Issue 1",10800);
+			timer.assocTask(task);
+			expect(timer.task).toBe(task);
+		});
+	});
+
+});
