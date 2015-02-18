125694285936bc286c47b7196e5f22c6a00a903b
Michael Hartung <hartungm4@gmail.com>
2015-02-18 17:35:17 -0500

    User Spec Updated and User class done

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,12 +1,13 @@
-describe("User Specifications", function() {
+describe("User Specifications | ", function() {
+	var user;
 
 	beforeEach(function() {
-		var user = User.init("Bob", 17.32, "Dev");
+		user = new User("Bob", 17.32, "Dev");
 	});
 
-	describe("Initalized user Bob", function() {
+	describe("Initalized user Bob | ", function() {
 		it("Users name is Bob", function() {
-			expect(user.userName).toBe("Bob");
+			expect(user.username).toBe('Bob');
 		});
 		it("Users payrate is $17.32", function() {
 			expect(user.payRate).toBe(17.32);
@@ -19,28 +20,50 @@ describe("User Specifications", function() {
 		});
 	});
 
-	describe("User changed Name", function(){
-		it("Bob changed his name to Sal", function() {
-			user.setName("Sal");
-			expect(user.userName).toBe("Sal");
+	describe("Initalized user Steve | ", function() {
+		var user2;
+		beforeEach(function() {
+			user2 = new User("Steve", 19.20, "QA")
+		});
+		it("User2's name is Steve", function() {
+			expect(user2.username).toBe('Steve');
+		});
+		it("First User's name should still be Bob", function() {
+			expect(user.username).toBe('Bob');
+		});
+		it("User2's payrate is $17.32", function() {
+			expect(user2.payRate).toBe(19.20);
+		});
+		it("User2's role is Dev", function() {
+			expect(user2.role).toBe("QA");
+		});
+		it("Users hours worked should start at 0", function() {
+			expect(user2.hoursWorked).toBe(0);
 		});
 	});
 
-	describe("User changed payRate", function() {
+	describe("User changed Name | ", function(){
+		it("Bob changed his name to Sal", function() {
+			user.setName("Sal");
+			expect(user.username).toBe("Sal");
+		});
+	});
+
+	describe("User changed payRate | ", function() {
 		it("Bob: pay increase to $20.00", function(){
 			user.setPayRate(20);
 			expect(user.payRate).toBe(20);
 		});
 	});
 
-	describe("User changed roles", function() {
+	describe("User changed roles | ", function() {
 		it("Bob decided to be a Scrum Master", function() {
 			user.setRole("SM");
 			expect(user.role).toBe("SM");
 		});
 	});
 
-	describe("User worked some hours", function() {
+	describe("User worked some hours | ", function() {
 		it("Bob worked 18.5 hours", function() {
 			user.addHoursWorked(18.5);
 			expect(user.hoursWorked).toBe(18.5);
@@ -49,7 +72,7 @@ describe("User Specifications", function() {
 			user.addHoursWorked(18.5);
 			expect(user.hoursWorked).toBe(18.5);
 			user.addHoursWorked(20);
-			expect(user.hoursWorked).toBe(20);
+			expect(user.hoursWorked).toBe(38.5);
 		});
 	});
 });
